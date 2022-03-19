bb77fa00b91232f327b6367771451d6ae199d5d9
jayur830 <jayur830@naver.com>
2022-03-20 01:44:53 +0900

    c

--- store/index.ts
+++ store/index.ts
@@ -1,11 +1,14 @@
 import { createWrapper } from "next-redux-wrapper";
 import { applyMiddleware, compose, createStore, Middleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 import reducer from "./reducer";
 
 const wrapper = createWrapper(() => {
     const middlewares: Middleware[] = [];
-    const enhancer = compose(applyMiddleware(...middlewares));
+    const enhancer = process.env.NODE_ENV === "production" ?
+        compose(applyMiddleware(...middlewares)) :
+        composeWithDevTools(applyMiddleware(...middlewares));
     // @ts-ignore
     return createStore(reducer, enhancer);
 }, { debug: process.env.NODE_ENV === "production" });
