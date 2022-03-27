b85e11f381f13ddfe395d578e1ee667027ff4487
L <owl8247@L-MacBookPro.local>
2022-03-27 22:49:44 +0900

    c

--- store/index.ts
+++ store/index.ts
@@ -1,16 +1,16 @@
 import { createWrapper } from "next-redux-wrapper";
 import { applyMiddleware, compose, createStore, Middleware } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
 
 import reducer from "./reducer";
 
-const wrapper = createWrapper(() => {
-    const middlewares: Middleware[] = [];
-    const enhancer = process.env.NODE_ENV === "production" ?
-        compose(applyMiddleware(...middlewares)) :
-        composeWithDevTools(applyMiddleware(...middlewares));
-    // @ts-ignore
-    return createStore(reducer, enhancer);
-}, { debug: process.env.NODE_ENV === "production" });
+const wrapper = createWrapper(
+	() => {
+		const middlewares: Middleware[] = [];
+		const enhancer = compose(applyMiddleware(...middlewares));
+		// @ts-ignore
+		return createStore(reducer, enhancer);
+	},
+	{ debug: process.env.NODE_ENV === "production" }
+);
 
 export default wrapper;
