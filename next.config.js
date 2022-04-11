0d115deaa44e943ffbcaae5fe78b4f1dd3f9ee21
enOlpaemi <jaeyeol.lee@enlighten.kr>
2022-04-11 14:58:37 +0900

    c

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
 	async headers() {
 		return [
 			{
-				source: '/api/graphql',
+				source: '/api/:path*',
 				headers: [
 					{ key: 'Access-Control-Allow-Credentials', value: 'true' },
 					{ key: 'Access-Control-Allow-Origin', value: '*' },
