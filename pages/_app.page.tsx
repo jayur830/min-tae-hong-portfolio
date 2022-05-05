cde993a9f8c5c39f6040c1d9fac224c0e01acb42
enlightenL <jaeyeol.lee@enlighten.kr>
2022-05-05 13:22:33 +0900

    added ConfigProvider of ant design

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -2,6 +2,7 @@
 import type { AppProps } from 'next/app';
 import { useRouter } from 'next/router';
 import { ApolloProvider } from '@apollo/client';
+import koKR from 'antd/lib/locale/ko_KR';
 import { ThemeProvider } from 'styled-components';
 
 // Global
@@ -10,6 +11,7 @@ import AppLayout from '@root/components/AppLayout';
 import AdminLayout from '@root/components/AdminLayout';
 import theme from '!!sass-variable-parser!@root/styles/variables.scss';
 import '@root/styles/globals.scss';
+import { ConfigProvider } from 'antd';
 
 // Local
 
@@ -18,13 +20,15 @@ function App({ Component, pageProps }: AppProps) {
 	const Layout = pathname.indexOf('/admin') !== -1 ? AdminLayout : AppLayout;
 
 	return (
-		<ApolloProvider client={client}>
-			<ThemeProvider theme={theme}>
-				<Layout>
-					<Component {...pageProps} />
-				</Layout>
-			</ThemeProvider>
-		</ApolloProvider>
+		<ConfigProvider locale={koKR} virtual>
+			<ApolloProvider client={client}>
+				<ThemeProvider theme={theme}>
+					<Layout>
+						<Component {...pageProps} />
+					</Layout>
+				</ThemeProvider>
+			</ApolloProvider>
+		</ConfigProvider>
 	);
 }
 
