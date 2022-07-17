62d76dcf99a49b2484b2813d54a59f8bc623e77d
jayur830 <jayur830@naver.com>
2022-07-17 15:34:37 +0900

    change meta tag

--- pages/_document.page.tsx
+++ pages/_document.page.tsx
@@ -40,7 +40,7 @@ export default class MyDocument extends Document {
 					<meta name="og:url" content="https://min-tae-hong-portfolio.herokuapp.com" />
 					<meta name="keywords" content="민태홍, 태홍, 포트폴리오, min tae hong, portfolio" />
 					<meta name="title" content="민태홍, 태홍, 포트폴리오, min tae hong, portfolio" />
-					<meta name="google-site-verification" content={process.env.GOOGLE_SITE_VERIFICATION} />
+					<meta name="google-site-verification" content="TWupcmG0uAjUfivvhR56XYgUWnYhaXnKShQEWY_TIMk" />
 					<link rel="icon" href="/favicon.ico" />
 				</Head>
 				<body>
